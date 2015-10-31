8d34c7f1baffa2446e7332f22cf47d54eb1d34d1
Gabriel Gonzalez <ggonzalez@twitter.com>
2015-10-30 19:34:36 -0700

    Cleaned up `Main`

--- Main.hs
+++ Main.hs
@@ -4,38 +4,67 @@ module Main where
 
 import Bot
 
-import Network.Xmpp
-
-import Control.Monad
-import Control.Exception (SomeException, catch)
+import Control.Monad (when, forever, void)
+import Control.Exception (Exception, SomeException, catch, throwIO)
+import Data.Monoid ((<>))
 import Data.Text (Text, pack, unpack)
-import Data.String
 import Data.IORef
-import System.Exit
-import Data.XML.Types (
-    nameLocalName, elementName, elementText
-  , Element(Element), Name(Name), Content(ContentText), Node(NodeContent)
-  )
-import Turtle (Parser, argText, options)
+import Data.XML.Types
+    ( elementText
+    , nameLocalName
+    , Element(..)
+    , Content(ContentText)
+    , Node(NodeContent)
+    )
+import Network.Xmpp
+    ( Message(..)
+    , Presence(..)
+    , Session
+    , answerMessage
+    , def
+    , getJid
+    , getMessage
+    , parseJid
+    , plain
+    , reconnectNow
+    , resourcepart
+    , sendMessage
+    , sendPresence
+    , session
+    , setConnectionClosedHandler
+    )
+import Turtle (Parser, argText, err, options, repr)
 
 mainLoop :: IORef Conf -> Text -> Session -> IO ()
 mainLoop conf xmpproom sess = do
-  msg <- getMessage sess
-  let from' = maybe "(anybody)" unpack (resourcepart =<< messageFrom msg)
-  let to' = maybe "(anybody)" unpack (resourcepart =<< messageTo msg)
-  let bodyElems = elems "body" msg
-  let delayElems = elems "delay" msg -- hipchat delayed messages
-  let responder = elems "responder" msg -- so you can't respond to yourself
-  when (null delayElems && (not . null) bodyElems && null responder) $ do
-    let body = head $ elementText (head bodyElems)
-    conf' <- readIORef conf
-    (replies, newConf) <- receiveMessage conf' (unpack xmpproom) from' to' (unpack body)
-    mapM_ (sendReply sess msg) replies
-    writeIORef conf newConf
-  return ()
+    msg <- getMessage sess
+    let from' = maybe "(anybody)" unpack (resourcepart =<< messageFrom msg)
+    let to'   = maybe "(anybody)" unpack (resourcepart =<< messageTo   msg)
+    let bodyElems  = elems "body"      msg
+    let delayElems = elems "delay"     msg -- hipchat delayed messages
+    let responder  = elems "responder" msg -- so you can't respond to yourself
+    case bodyElems of
+        [bodyElem] -> do
+            when (null delayElems && null responder) $ do
+                let body = head (elementText bodyElem)
+                conf' <- readIORef conf
+                (replies, newConf) <- receiveMessage
+                    conf'
+                    (unpack xmpproom)
+                    from'
+                    to'
+                    (unpack body)
+                mapM_ (sendReply sess msg) (map pack replies)
+                writeIORef conf newConf
+        _      ->
+            err ("Error: Unexpected number of message body elements\n\
+                 \n\
+                 \Message body elements: " <> repr bodyElems <> "\n\
+                 \Expected # of elements: 1\n\
+                 \Actual   # of elements: " <> repr (length bodyElems) )
 
 data Options = Options
-    { hostname :: Text
+    { host     :: Text
     , xmppid   :: Text
     , xmpppass :: Text
     , xmppnick :: Text
@@ -52,53 +81,61 @@ opts =  Options
 
 main :: IO ()
 main = do
-  Options {..} <- options "Haskell-driven HipChat bot" opts
+    Options {..} <- options "Haskell-driven HipChat bot" opts
   
-  c' <- mkConf
-  conf <- newIORef c'
+    c'   <- mkConf
+    conf <- newIORef c'
   
-  esess <- session (fromString (unpack hostname)) (Just (\_ -> ([plain xmppid Nothing xmpppass]), Nothing)) def
-  sess <- case esess of
-    Right s -> return s
-    Left e -> putStrLn ("XmppFailure: " ++ show e) >> exitWith (ExitFailure 1)
-  sendMUCPresence (unpack xmpproom) (unpack xmppnick) sess
-  
-  setConnectionClosedHandler (\_ _ -> do
-    _ <- reconnectNow sess
-    sendMUCPresence (unpack xmpproom) (unpack xmppnick) sess) sess
-  forever (catch (mainLoop conf xmpproom sess) handler)
+    sess <- throws
+        (session
+            (unpack host)
+            (Just (\_ -> ([plain xmppid Nothing xmpppass]), Nothing))
+            def )
 
-sendReply :: Session -> Message -> String -> IO ()
+    sendMUCPresence (unpack xmpproom) (unpack xmppnick) sess
+  
+    setConnectionClosedHandler (\_ _ -> do
+        _ <- reconnectNow sess
+        sendMUCPresence (unpack xmpproom) (unpack xmppnick) sess) sess
+    forever (catch (mainLoop conf xmpproom sess) handler)
+
+throws :: Exception e => IO (Either e a) -> IO a
+throws io = do
+    x <- io
+    case x of
+        Left e  -> throwIO e
+        Right a -> return a
+
+sendReply :: Session -> Message -> Text -> IO ()
 sendReply sess msg content = do
-  case answerMessage msg [
-    Element "body" [] [
-      NodeContent (ContentText (pack content))]
-    ] of
-      Just answer -> sendMessage answer sess >> return ()
-      Nothing -> return ()
+    let element = Element
+            { elementName = "body"
+            , elementAttributes = []
+            , elementNodes = [NodeContent (ContentText content)]
+            }
+    case answerMessage msg [element] of
+        Just answer -> throws (sendMessage answer sess)
+        Nothing     -> return ()
 
 handler :: SomeException -> IO ()
 handler = print
 
 elems :: Text -> Message -> [Element]
-elems tagname mes = filter ((== tagname) . nameLocalName . elementName) $
-                           (messagePayload mes)
+elems tagname msg =
+    filter ((== tagname) . nameLocalName . elementName) (messagePayload msg)
 
 sendMUCPresence :: String -> String -> Session -> IO ()
 sendMUCPresence xmpproom xmppnick sess = do
-  jid' <- getJid sess
-  void $ sendPresence (def {
-      presenceFrom = jid'
-    , presenceTo = Just (parseJid (xmpproom ++ '/' : xmppnick))
-    , presencePayload = [Element "x" [(Name "xmlns" Nothing Nothing, [ContentText "http://jabber.org/protocol/muc"])] []]
-    }) sess
-
-getOrElse :: [String] -> Int -> IO Text
-getOrElse xs i =
-  if length xs >= i
-    then return $ pack $ xs !! i
-    else printUsage >> exitWith (ExitFailure 1)
-
-printUsage :: IO ()
-printUsage =
-  putStrLn "USAGE: tau HOSTNAME XMPPID XMPPPASS XMPPNICK XMPPROOM"
+    jid' <- getJid sess
+    let element = Element
+            { elementName       = "x"
+            , elementAttributes =
+                [("xmlns" , [ContentText "http://jabber.org/protocol/muc"])]
+            , elementNodes      = []
+            }
+    let presence = def
+            { presenceFrom    = jid'
+            , presenceTo      = Just (parseJid (xmpproom ++ '/' : xmppnick))
+            , presencePayload = [element]
+            }
+    void (sendPresence presence sess)
